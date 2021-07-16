e8c5449738615433061cad37fe6ece2ff6b0179c
PanicS7 <55251860+PanicS7@users.noreply.github.com>
2021-07-16 19:19:15 +0200

    HTML, CSS/SCSS, JavaScript/JQuery, Bootstrap, animate.css

---
+++ Udemy 14/app.js
@@ -0,0 +1,21 @@
+﻿$(window).scroll(function () {
+    if (window.scrollY > 600) {
+        $("#imageContainer2").css("display", "block");
+        $("#imageContainer2").addClass("animate__animated animate__fadeInUp");
+    }
+
+    if (window.scrollY > 1100) {
+        $("#imageContainer3").css("display", "block");
+        $("#imageContainer3").addClass("animate__animated animate__fadeInUp");
+    }
+    if (window.scrollY > 2000) {
+        $("#section5 div ul li").css("visibility", "visible");
+        $("#section5 div ul li:nth-child(1)").addClass("animate__animated animate__slideInLeft");
+        $("#section5 div ul li:nth-child(2)").addClass("animate__animated animate__slideInLeft");
+
+        $("#section5 div ul li:nth-child(3)").addClass("animate__animated animate__slideInRight");
+        $("#section5 div ul li:nth-child(4)").addClass("animate__animated animate__slideInRight");
+    }
+});
+
+
